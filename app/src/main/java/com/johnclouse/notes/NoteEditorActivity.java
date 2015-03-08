c101cb4ec2c116e7f950367102d2eb054f9e22e5
John Clouse <clous0dd@gmail.com>
2015-03-07 23:44:03 -0700

    Editor activity

--- app/src/main/java/com/johnclouse/notes/NoteEditorActivity.java
+++ app/src/main/java/com/johnclouse/notes/NoteEditorActivity.java
@@ -1,48 +1,24 @@
 package com.johnclouse.notes;
 
-import android.app.ListActivity;
-import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-
-import com.johnclouse.notes.data.NoteItem;
-import com.johnclouse.notes.data.NotesDataSource;
-
-import java.util.List;
 
 
-public class MainActivity extends ListActivity {
-
-    private NotesDataSource dataSource;
-    List<NoteItem> notesList;
+public class NoteEditorActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        dataSource = new NotesDataSource(this); //"this" needed if there is a non-default constr.
-
-        refreshDisplay();
-
-    }
-
-    private void refreshDisplay() {
-        notesList = dataSource.findall();
-        ArrayAdapter<NoteItem> adapter =
-                new ArrayAdapter<NoteItem>(this, R.layout.list_item_layout, notesList);
-        setListAdapter(adapter);
+        setContentView(R.layout.activity_note_editor);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_note_editor, menu);
         return true;
     }
 
