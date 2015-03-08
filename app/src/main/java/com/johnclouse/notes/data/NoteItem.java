58ef856d8ef8fefb31a9171a5cf1079ce786bcd4
John Clouse <clous0dd@gmail.com>
2015-03-07 18:23:56 -0700

    Filled out NoteItem

--- app/src/main/java/com/johnclouse/notes/data/NoteItem.java
+++ app/src/main/java/com/johnclouse/notes/data/NoteItem.java
@@ -1,5 +1,9 @@
 package com.johnclouse.notes.data;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by John on 3/7/2015.
  */
@@ -18,6 +22,21 @@ public class NoteItem {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public static NoteItem getNew() {
+        Locale locale = new Locale("en_US"); // For easily sorted timestamp
+        Locale.setDefault(locale);
+
+        String pattern = "yyyy-MM-dd HH:mm:ss Z"; // Z adds offset from GMT
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        String key = formatter.format(new Date());
+
+        NoteItem note = new NoteItem();
+        note.setKey(key);
+        note.setText("");
+        return note;
+
     }
 
     private String key;
